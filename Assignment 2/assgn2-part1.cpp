fdcf3681e5baba5d29468e562bb09a616dbf7428
Shubham Karpe <shubham412007.digya@gmail.com>
2026-09-18 14:14:45 +0530

    Update assgn2-part1.cpp

--- Assignment 2/assgn2-part1.cpp
+++ Assignment 2/assgn2-part1.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 int main()
 {
- int rno[5];
- cout << "Enter Roll No. of 5 Students:"<<endl;
- for (int i = 0; i < 5; i++)
-  {
-   cout<<"Student "<< i+1 <<" :";
-   cin >> rno[i];
-  }
- cout << "\nRoll No. of students: "<<endl;
-    for(int i=0;i<5;i++)
-        {
-        cout<<rno[i]<<endl;
-        }
+    int rno[5];
+    cout << "Enter Roll No. of 5 Students:" << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Student " << i + 1 << " :";
+        cin >> rno[i];
+    }
+    cout << "\nRoll No. of students: " << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << rno[i] << endl;
+    }
     return 0;
 }
